b5c8c154c5f35b7529bbb69b6c262f6dfc0e1338
Tony Messias <tonyzrp@gmail.com>
2015-11-06 21:29:10 -0300

    add test to add a new todo

--- app/src/androidTest/java/com/example/tony/todoapp/TodoAppMainActivityTest.java
+++ app/src/androidTest/java/com/example/tony/todoapp/TodoAppMainActivityTest.java
@@ -1,9 +1,21 @@
 package com.example.tony.todoapp;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ActivityInstrumentationTestCase2;
 
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class TodoAppMainActivityTest extends ActivityInstrumentationTestCase2<TodoAppMainActivity> {
     private TodoAppMainActivity todoMainActivity;
@@ -19,7 +31,20 @@ public class TodoAppMainActivityTest extends ActivityInstrumentationTestCase2<To
     }
 
     @Test
-    public void testWorks() {
-        assertTrue(true);
+    public void testAddsNewTodo() {
+        onView(withId(R.id.fab))
+            .perform(ViewActions.click());
+
+        onView(withId(R.id.name))
+            .perform(ViewActions.typeText("New todo"));
+
+        onView(withId(R.id.description))
+            .perform(ViewActions.typeText("New Description"));
+
+        onView(withId(R.id.save))
+            .perform(ViewActions.click());
+
+        onView(withText("New todo"))
+            .check(matches(isDisplayed()));
     }
 }
